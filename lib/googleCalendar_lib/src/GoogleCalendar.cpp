e8e05f16c512ce95b221ef4f03d1b76abc619494
camillemarius <camillemariuschatton@gmail.com>
2026-09-08 15:29:52 +0200

    Simplified filtering of unwanted canceled events.

--- lib/googleCalendar_lib/src/GoogleCalendar.cpp
+++ lib/googleCalendar_lib/src/GoogleCalendar.cpp
@@ -211,10 +211,9 @@ bool GoogleCalendar::getEvents(const String& calendarId, std::vector<CalendarEve
     events.clear();
 
     for (JsonObject item : items) {
+        
         // Abgesagte Events nicht an das Display weitergeben
-        const char* status = item["status"] | "";
-        if (strcmp(status, "cancelled") == 0) {
-            LOG_FS_DEBUG("Cancelled Event übersprungen: %s", item["summary"] | "Ohne Titel");
+        if (item["status"] == "cancelled") {
             continue;
         }
 
